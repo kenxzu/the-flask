21ab354ca5fe635ce9da6317f77520759c061d86
KenFoster <kenmorintoh96@gmail.com>
2025-09-22 11:42:03 +0700

    done with the "bigger picture" of the flask development

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from config import Config
+from extension import db
+
+
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+
+        
+from routes.main_routes import main
+
+app.register_blueprint(main)
